0664b5e0254cc60fbafe75981a0853d072c7c3a0
4everblue77 <109893202+4everblue77@users.noreply.github.com>
2025-10-17 12:36:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ for section in sections:
         
         st.query_params["day"] = selected_day
         st.query_params["section"] = section
+              st.write(f"DEBUG: Set query params → day: {selected_day}, section: {section}")
         st.rerun()  # Refresh to apply query params
 
         st.switch_page("pages/details.py")
